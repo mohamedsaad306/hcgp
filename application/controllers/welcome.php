53cd48a4eeabd43e630b85484347cc1fda3b2b1c
mohamedsaad306 <makcemos_X@yahoo.com>
2015-02-11 23:02:22 +0200

    new line
    katbt new line

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,7 +20,8 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-            echo "this is index function ";
+            //echo "this is index function ";
+            echo 'this is new echo ';
 	}
 }
 
