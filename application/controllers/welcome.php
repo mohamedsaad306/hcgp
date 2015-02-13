80fe9bc88f314abda7867f5fc05c46d41c8549ef
mohamedsaad306 <makcemos_X@yahoo.com>
2015-02-13 21:10:58 +0200

    w_controller

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -20,8 +20,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		  $this->load->view('welcome_message');
-            //echo "this is index function ";
-        //    echo 'this is new echo ';
+         
 	}
 }
 
