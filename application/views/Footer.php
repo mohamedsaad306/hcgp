4c92e20564c395023d225465d1b9e3decd731be8
mohamedsaad306 <fallen.skyman@gmail.com>
2015-02-14 18:47:02 +0200

    updated footer
    ana katbt 2014-2015  fe elfooter 3ashan tb2a a7sn

--- application/views/Footer.php
+++ application/views/Footer.php
@@ -6,7 +6,7 @@
 <div class="row" >
     <div class="span12"  >
         <div class="well">
-            <center> Health Circle ®</center>
+            <center> Health Circle 2014-2015 ®</center>
         </div>
     </div>
 </div>
